6ce7e5dc3d944cac2649e6fc285d246b5a738ceb
Tharanish <tharanishvaranr@gmail.com>
2026-08-19 22:34:36 +0530

    Fix Start Command WSGI alias application = app and Procfile

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
 from app import create_app
 
 app = create_app()
+application = app  # WSGI alias for cloud web servers
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
